4468d5dadd24c6fab8536b1c3e534fa698cb299a
iamyourcat <iamyourcatdev@gmail.com>
2020-08-18 18:59:30 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# node-proxy
+node-proxy is a [node.js](https://nodejs.org) tiny proxy server without any external dependencies.
+# Config file (config.json)
+```json
+{
+  "defaultPort": 8080
+} 
+```
+By default, node-proxy uses value of environment variable PORT. If it's not set, config->defaultPort is used.
+# 
+Project owner - iamyourcat. License - [ISC](https://opensource.org/licenses/ISC).
---
+++ index.js
@@ -0,0 +1,33 @@
+var http = require('http');
+
+const config = require("./config.json");
+const port = process.env.PORT || config.defaultPort;
+
+http.createServer(onRequest).listen(port);
+console.log("Proxy server is running. Using port: "+port+".");
+
+function getHostname(url){
+	res = url;
+	if(res.startsWith("https://"))res=res.slice(8);
+	if(res.startsWith("http://"))res=res.slice(7);
+	return res.split("/")[0];
+}
+
+function onRequest(client_req, client_res) {
+  var options = {
+    hostname: getHostname(client_req.url),
+    port: 80,
+    path: client_req.url,
+    method: client_req.method,
+    headers: client_req.headers
+  };
+  var proxy = http.request(options, function (res) {
+    client_res.writeHead(res.statusCode, res.headers)
+    res.pipe(client_res, {
+      end: true
+    });
+  });
+  client_req.pipe(proxy, {
+    end: true
+  });
+}
